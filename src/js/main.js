6828b8838e7d301f24519def37a2bf4e47bad269
Dillon Estrada <dillon.estrada55@gmail.com>
2021-09-22 08:55:14 -0500

    Add jump.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
-import "../scss/main.scss";
+import '../scss/main.scss';
+import jump from 'jump.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const navbar = document.querySelector('#navbar');
+  const navbarHeight = navbar.offsetHeight;
+  document.querySelectorAll('.nav-link').forEach((link) => {
+    link.addEventListener('click', (event) => {
+      const id = event.target.dataset.scroll;
+      jump(id, { offset: -navbarHeight, duration: 500 });
+    });
+  });
+});
